e9a23021bca01446c56314ee34b88d2355ebaeb4
Danieboy <danne467@hotmail.com>
2015-06-10 15:54:45 +0200

    Draw Thread Boxes
    Multithreading example for C#

---
+++ Draw thread boxes/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Draw_thread_boxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+        }
+
+        Thread rThread;
+        Thread bThread;
+        Thread gThread;
+        Random random;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            rThread = new Thread(redThread);
+            rThread.Start();
+        }
+
+        public void redThread()
+        {
+            for (int i = 0; i < 75; i++)
+            {
+                    this.CreateGraphics().DrawRectangle(new Pen(Brushes.Red, 4), new Rectangle(random.Next(0, this.Width), (random.Next(0, this.Height)), 20, 20));
+                    Thread.Sleep(75);
+            }
+
+            MessageBox.Show("Completed Drawing 75 Red Boxes.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bThread = new Thread(blueThread);
+            bThread.Start();
+        }
+
+        public void blueThread()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Blue, 4), new Rectangle(random.Next(0, this.Width), (random.Next(0, this.Height)), 20, 20));
+                Thread.Sleep(50);
+            }
+
+            MessageBox.Show("Completed Drawing 100 Blue Boxes.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            {
+                gThread = new Thread(greenThread);
+                gThread.Start();
+            }
+        }
+
+        public void greenThread()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                this.CreateGraphics().DrawRectangle(new Pen(Brushes.Green, 8), new Rectangle(random.Next(0, this.Width), (random.Next(0, this.Height)), 20, 20));
+                Thread.Sleep(200);
+            }
+
+            MessageBox.Show("Completed Drawing 25 Green Boxes.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ Draw thread boxes/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Draw thread boxes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Draw thread boxes")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bc722c46-a14d-4326-bb67-efae19c31f06")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
